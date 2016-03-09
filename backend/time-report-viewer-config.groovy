91a2d3ba0616a2f6eaaafc7614eeb6c8221b5719
Ceras <fredrik_orback@hotmail.com>
2016-03-09 10:32:15 +0100

    fixed file export and updated config for new server

--- backend/time-report-viewer-config.groovy
+++ backend/time-report-viewer-config.groovy
@@ -3,7 +3,7 @@ dropbox.time_report.folder.url = 'https://www.dropbox.com/sh/eu5p1e5xwer3skp/AAA
 
 grails.plugin.springsecurity.providerNames = ['ldapAuthProvider'] // specify this when you want to skip attempting to load from db and only use LDAP
 grails.plugin.springsecurity.password.algorithm = 'SHA' // found SHA from confluence
-grails.plugin.springsecurity.ldap.context.server = 'ldap://dev.find-out.se:389'
+grails.plugin.springsecurity.ldap.context.server = 'ldap://localhost:389'
 
 grails.plugin.springsecurity.ldap.context.managerDn = 'cn=admin,dc=find-out,dc=se' // verified
 grails.plugin.springsecurity.ldap.context.managerPassword = 'fLossec' // verified
